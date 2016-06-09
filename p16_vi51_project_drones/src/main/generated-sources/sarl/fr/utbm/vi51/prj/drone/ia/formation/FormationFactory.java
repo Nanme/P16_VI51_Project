a98cb615b75281814e37faf088f352d1ff5bc3b3
Nanme <real90@hotmail.fr>
2016-06-09 17:15:13 +0200

    Remove generated sources \o/

--- p16_vi51_project_drones/src/main/generated-sources/sarl/fr/utbm/vi51/prj/drone/ia/formation/FormationFactory.java
+++
@@ -1,8 +0,0 @@
-package fr.utbm.vi51.prj.drone.ia.formation;
-
-import fr.utbm.vi51.prj.drone.ia.formation.Formation;
-
-@SuppressWarnings("all")
-public interface FormationFactory {
-  public abstract Formation build(final int nbAnchors);
-}
